76e9ca0a8aa6864d5d4b0fe8c746fa77fd69fb7b
genrosales11-minjcA <genrosales11@gmail.com>
2022-08-26 16:22:47 -0700

    signup controller

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: [({ length }) => length >= 6, "Password should be longer."]
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    }
+});
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+});
+  
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+  
+const User = model('User', userSchema);
+
+module.exports = User;
