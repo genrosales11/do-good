ac08bf32e464546effe9ef4c7a1e957cd66e677a
genrosales11-minjcA <genrosales11@gmail.com>
2022-08-26 16:19:13 -0700

    login-controller.js

---
+++ server/controllers/login-controller.js
@@ -0,0 +1,20 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
+
+module.exports = {
+    async authenticateUser({ body }, res) {
+        const user = await User.findOne({ username: body.username })
+
+        if (!user) {
+            return res.status(400).json({ message: 'Unable to find user'});
+        }
+        const correctPw = await user.isCorrectPassword(body.password);
+
+        if (!correctPw) {
+            return res.status(400).json({ message: 'Password Incorrect'})
+        }
+        
+        const token = signToken(user);
+        return res.status(200).json({ token, user})
+    }
+}
