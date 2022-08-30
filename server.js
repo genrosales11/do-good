a3952e5d2f74eb9929d4b3a7531228945355be05
darmstrong <armstrong2579@gmail.com>
2022-08-30 15:34:11 -0700

    add imports

--- server.js
+++ server.js
@@ -2,3 +2,25 @@
 
 // const weather = { weather: async (query) =>
 //  axios.get(``) }
+
+// Import express and Apollo Server to create our server
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+//   change this if named smth else
+  context: authMiddleware
+});
+
+const app = express();
+
+
