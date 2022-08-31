d0eef548629d0269e4ae9ff2fc99f4e9fd6a5da8
darmstrong <armstrong2579@gmail.com>
2022-08-31 10:33:48 -0700

    setup typedefs for models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
 
+  type Task {
+    _id: ID
+    taskText: String
+    taskValue: Int
+    category: Category
+  }
+
+  type History {
+    _id: ID
+    date: Date
+    tasks: [Task]
+  }
+
+  type Goal {
+    _id: ID
+    goalText: String
+    goalValue: Int
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    username: String
+    password: String
+    email: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+
+  }
 `;
 
 module.exports = typeDefs;
