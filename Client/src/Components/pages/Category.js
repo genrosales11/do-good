c43d5934ee251f8ecf1608b344099e5b75923fe5
DNA601 <dnavarrete6001@gmail.com>
2022-08-30 18:20:05 -0700

    making layout for tasks at category.js

---
+++ Client/src/Components/pages/Category.js
@@ -0,0 +1,17 @@
+export default function Category() {
+    return (
+
+      <body class="categoryBody">
+<header>
+<h1>Category</h1>
+</header>
+  
+  <main>
+
+  </main>
+         
+      </body>
+    
+
+    );
+  }
