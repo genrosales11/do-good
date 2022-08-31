fefabb7f764b0f0834fa89b9dd4666e86573fac4
genrosales11-minjcA <genrosales11@gmail.com>
2022-08-31 14:12:17 -0700

    /login

--- server/routes/api/login-routes.js
+++
@@ -1,8 +0,0 @@
-
-const router = require('express').Router();
-
-const { authenticateUser } = require('../../controllers/login-controller');
-
-router.route('/').post(authenticateUser);
-
-module.exports = router;
