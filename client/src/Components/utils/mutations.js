b9ffd8e61b4c70150d9e6026db9a5d6513247f9e
darmstrong <armstrong2579@gmail.com>
2022-08-31 22:31:10 -0700

    import apollo client and create exports

---
+++ client/src/Components/utils/mutations.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+
+`;
+
+export const ADD_USER = gql`
+
+`;
+
+export const ADD_TASK = gql`
+`;
+
+export const ADD_GOAL = gql`
+`;
+
+export const UPDATE_TASK = gql`
+`;
+
+export const REMOVE_TASK = gql`
+`;
+
+export const REMOVE_GOAL = gql`
+`;
