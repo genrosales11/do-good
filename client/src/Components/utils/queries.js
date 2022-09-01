66c5de01f2f33b7f8693ffc1d22a1b0b5d7d9b09
darmstrong <armstrong2579@gmail.com>
2022-08-31 22:18:40 -0700

    add user query

--- client/src/Components/utils/queries.js
+++ client/src/Components/utils/queries.js
@@ -1,13 +1,21 @@
 import { gql } from '@apollo/client';
 
 // Do we need to query all of them or by group somehow
-export const QUERY_ALL_TASKS = gql`
-`;
 
 export const QUERY_TASK = gql`
+
 `;
 
 export const QUERY_USER = gql`
+{
+    user {
+      username
+      tasks {
+        _id
+        taskText
+      }
+    }
+  }
 `;
 
 export const QUERY_HISTORY = gql`
