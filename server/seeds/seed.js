b841e99b2b5c3ad6e24607bf143dd95e0fdd7386
darmstrong <armstrong2579@gmail.com>
2022-09-02 13:44:37 -0700

    set up imports and delete pre-existing users/ tasks to avoid duplicate issues

---
+++ server/seeds/seed.js
@@ -0,0 +1,23 @@
+const db = require('../config/connection');
+const { User, Task } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const taskSeeds = require('./taskSeeds.json');
+
+db.once('open', async () => {
+  try {
+    // Deleting our tasks and users first so we don't create duplicates
+    await Task.deleteMany({});
+    await User.deleteMany({});
+
+    // Creating our users
+    await User.create(userSeeds);
+
+    // mass create tasks? are we assigning them to anywhere here?
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
