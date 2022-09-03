747b49252b54fd6b698c5a789ce56e6f9a794264
OnlyMeHere <bennett45.j.m@gmail.com>
2022-09-03 13:24:31 -0700

    add image to Home.js using CSS

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -1,5 +1,19 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
 export default function Home() {
     return (
-     <h1>Home</h1>
+      <Container className="backGroundImg" >
+
+      <div>
+        <h1>Home</h1>
+      </div>
+      <div>
+        <p>With this app you can track your efforts to save the world</p>
+      </div>
+
+      </Container>
+
+
     );
-  }
+};
