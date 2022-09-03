4f4a6a7792750d3b65d1fca1873e0f06f7e3dd56
genrosales11-minjcA <genrosales11@gmail.com>
2022-09-02 18:12:04 -0700

    adding recycle group

--- client/src/Components/pages/Recycle.js
+++ client/src/Components/pages/Recycle.js
@@ -1,14 +1,18 @@
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 function DefaultExample() {
   return (
+    <Container style={{ alignItems: 'center' }}>
     <ListGroup as="ol" numbered>
       <ListGroup.Item
         as="li"
         className="d-flex justify-content-between align-items-start"
       >
-        <div className="ms-2 me-auto">
+        <div className="md-2 me-auto">
           <div className="fw-bold">Clean containers out before recycling.</div>
           
         </div>
@@ -41,6 +45,7 @@ function DefaultExample() {
         </Badge>
       </ListGroup.Item>
     </ListGroup>
+    </Container>
   );
 }
 
