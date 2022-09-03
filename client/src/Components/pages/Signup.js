371133052b4a07dd4cf49620a99e4e2dc353d039
DNA601 <dnavarrete6001@gmail.com>
2022-09-02 18:34:12 -0700

    login and signup layout

--- client/src/Components/pages/Signup.js
+++ client/src/Components/pages/Signup.js
@@ -1,11 +1,64 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+//  import { Link } from 'react-router-dom';
+//  import { useMutation } from '@apollo/client';
+//  import { ADD_PROFILE } from '../utils/mutations';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+// import Auth from '../utils/auth';
 
-import { useMutation } from '@apollo/client';
-import { ADD_PROFILE } from '../utils/mutations';
 
-export default function Signup() {
-    return (
-     <h1>Signup</h1>
-    );
-  }
+
+
+const Signup = () => {
+ 
+
+  return (
+
+    <Form className='signUp'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label> Create a Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+  );
+};
+
+export default Signup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
