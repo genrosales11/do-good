64efeb1a062412581f9bfe3276cfb0d557047c19
OnlyMeHere <bennett45.j.m@gmail.com>
2022-09-05 11:07:44 -0700

    add images to About.js and CSS

--- client/src/Components/pages/About.js
+++ client/src/Components/pages/About.js
@@ -8,58 +8,61 @@ import damienArmstrong from "../images/damienArmstrong.png";
 import jamesBennett from "../images/jamesBennett.png";
 
 export default function About() {
+  
     return (
-     <Container>
-      <div className="row no-gutters">
-      <Col className="col-sm">
-      <Card style={{ height: '18rem', width: '18rem' }}>
-        <Card.Img className="card-img-top" src={ genesisRosales.png } />
-        <Card.Body>
-        <Card.Title>Genesis Rosales</Card.Title>
-          <Card.Text>
-            Graduate of the UC Berkeley Full Stack Coding Bootcamp
-          </Card.Text>
-          {/* <Button variant="info"> <Link to="github">GitHub Link</Link></Button> */}
-        </Card.Body>
-      </Card>
-      </Col>
-      <Col className="col-sm">
-      <Card style={{ height: '18rem', width: '18rem' }}>
-        <Card.Img variant="top" src={ davidNavarreteAyala.png } />
-        <Card.Body>
-        <Card.Title>David Navarrete Ayala</Card.Title>
-          <Card.Text>
-            Graduate of the UC Berkeley Full Stack Coding Bootcamp
-          </Card.Text>
-          {/* <Button variant="info"> <Link to="github">GitHub link</Link></Button> */}
-        </Card.Body>
-      </Card>
-      </Col>
-      <Col className="col-sm">
-      <Card style={{ height: '18rem', width: '18rem' }}>
-        <Card.Img variant="top" src={ damienArmstrong.png } />
-        <Card.Body>
-        <Card.Title>Damien Armstrong</Card.Title>
-          <Card.Text>
-            Graduate of the UC Berkeley Full Stack Coding Bootcamp
-          </Card.Text>
-          {/* <Button variant="info"> <Link to="github">GitHub Link</Link></Button> */}
-        </Card.Body>
-      </Card>
-      </Col>
-      <Col className="col-sm">
-      <Card style={{ height: '18rem', width: '18rem' }}>
-        <Card.Img variant="top" src={ jamesBennett.png } />
-        <Card.Body>
-        <Card.Title>James Bennett</Card.Title>
-          <Card.Text>
-            Graduate of the UC Berkeley Full Stack Coding Bootcamp
-          </Card.Text>
-          {/* <Button variant="info"> <Link to="github">GitHub link</Link></Button> */}
-        </Card.Body>
-      </Card>
-      </Col>
-      </div>
-     </Container>
+      <body className="data-spy='scroll' data-target='#aboutCards'">
+      <Container>
+        <div id="aboutCards" className="row no-gutters">
+        <Col className="col-sm">
+        <Card style={{ height: '18rem', width: '18rem' }}>
+          <Card.Img className="top" src={ genesisRosales.png } alt='image of Genesis' />
+          <Card.Body>
+          <Card.Title>Genesis Rosales</Card.Title>
+            <Card.Text>
+              Graduate of the UC Berkeley Full Stack Coding Bootcamp
+            </Card.Text>
+            {/* <Button variant="genbtn"> <Link to="github">GitHub Link</Link></Button> */}
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col className="col-sm">
+        <Card style={{ height: '18rem', width: '18rem' }}>
+          <Card.Img variant="top" src={ davidNavarreteAyala.png } alt='image of David' />
+          <Card.Body>
+          <Card.Title>David Navarrete Ayala</Card.Title>
+            <Card.Text>
+              Graduate of the UC Berkeley Full Stack Coding Bootcamp
+            </Card.Text>
+            {/* <Button variant="davbtn"> <Link to="github">GitHub link</Link></Button> */}
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col className="col-sm">
+        <Card style={{ height: '18rem', width: '18rem' }}>
+          <Card.Img variant="top" src={ damienArmstrong.png } alt='image of Damien' />
+          <Card.Body>
+          <Card.Title>Damien Armstrong</Card.Title>
+            <Card.Text>
+              Graduate of the UC Berkeley Full Stack Coding Bootcamp
+            </Card.Text>
+            {/* <Button variant="dambtn"> <Link to="github">GitHub Link</Link></Button> */}
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col className="col-sm">
+        <Card style={{ height: '18rem', width: '18rem' }}>
+          <Card.Img variant="top" src={ jamesBennett.png } alt='image of James' />
+          <Card.Body>
+          <Card.Title>James Bennett</Card.Title>
+            <Card.Text>
+              Graduate of the UC Berkeley Full Stack Coding Bootcamp
+            </Card.Text>
+            {/* <Button variant="jambtn"> <Link to="github">GitHub link</Link></Button> */}
+          </Card.Body>
+        </Card>
+        </Col>
+        </div>
+      </Container>
+     </body>
     );
   }
