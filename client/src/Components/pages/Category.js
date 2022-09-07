02b8e42507e575127f3f62b6efd3b16e674c2502
genrosales11-minjcA <genrosales11@gmail.com>
2022-09-06 21:50:51 -0700

    adding new images

--- client/src/Components/pages/Category.js
+++ client/src/Components/pages/Category.js
@@ -112,7 +112,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 import recycle from "../images/recycle.png";
 import trash from "../images/trash.png";
-import transportation from "../images/transportation.png";
+import transportation from "../images/cycle.png";
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom'
 
@@ -127,33 +127,36 @@ export default function App() {
             className="d-block w-60" src={recycle} style={{ height: 505 }}
             alt="recycle"
           />
-          <Button variant="info"> <Link to="/recycle">Go to Recycle</Link></Button>
+          {/* <Button variant="info"> <Link to="/recycle">Go to Recycle</Link></Button> */}
           <Carousel.Caption>
             <h3>Recycle</h3>
             <p> Reducing, reusing, and recycling cuts down on the amount of raw material 
          needed to create new products, lessening the overall impact on natural resources.</p>
+         <Button variant="info"> <Link to="/recycle">Go to Recycle</Link></Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-60" src={trash} style={{ height: 555 }}
             alt="trash"/>
-          <Button variant="info"> <Link to="/trash">Pick up trash</Link></Button>
+          {/* <Button variant="info"> <Link to="/trash">Pick up trash</Link></Button> */}
           <Carousel.Caption>
             <h3>Pick up trash</h3>
             <p>Every piece of litter intercepted from the beach is one less piece of
                litter that doesn't end up in the ocean</p>
+               <Button variant="info"> <Link to="/trash">Pick up trash</Link></Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item >
           <img
             className="d-block w-60" src={transportation} style={{ height: 505 }}
             alt="trash"/>
-          <Button variant="info"> <Link to="/transportation">Reduce Your Carbon Footprint</Link></Button>
+          {/* <Button variant="info"> <Link to="/transportation">Reduce Your Carbon Footprint</Link></Button> */}
           <Carousel.Caption>
             <h3>Reduce Your Carbon Footprint</h3>
             <p> Cycling improves cardio-vascular and aerobic fitness, lowers blood pressure, 
         boosts energy, builds muscle, and improves coordination. </p>
+        <Button variant="info"> <Link to="/transportation">Reduce Your Carbon Footprint</Link></Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
