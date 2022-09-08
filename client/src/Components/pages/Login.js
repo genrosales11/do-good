fdb589b9ad6cebf2ffd663ad86bd1bd766df82ef
DNA601 <dnavarrete6001@gmail.com>
2022-09-07 17:30:04 -0700

    changing and cleanup

--- client/src/Components/pages/Login.js
+++ client/src/Components/pages/Login.js
@@ -50,7 +50,7 @@ const Login = () => {
         <Form.Label>Email address</Form.Label>
         <Form.Control   value={formState.email} onChange={handleChange} type="email" name="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+    
         </Form.Text>
       </Form.Group>
 
